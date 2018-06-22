711189918f6b89d3661abd950a35f8e9a6afab80
PhiColTan <DamLaR1803@gmail.com>
2018-06-22 15:42:38 -0300

    Add function to send emails using the settings in config.json

---
+++ notification/__init__.py
@@ -0,0 +1,36 @@
+
+# Name: Haar Cascade Creation
+# Desc: Creation of custom Haar Cascades Classifiers
+# Repo: https://github.com/IamPhytan/haar-cascade-creation
+# Author: IamPhytan
+# License: MIT
+# Path: -/notification
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def send_email(config, subject, body):
+
+    if config['EMAIL']['WITH_EMAIL_NOTIFICATIONS']:
+
+        fromaddr = 'Haar Cascade Creation'
+        recipients = config['EMAIL']['EMAIL_TO']
+        cc_recipients = config['EMAIL']['EMAIL_CC']
+        login = config['EMAIL']['EMAIL_LOGIN']
+        password = config['EMAIL']['EMAIL_PASSWORD']
+
+        msg = MIMEMultipart()
+        msg['From'] = fromaddr
+        msg['To'] = ', '.join(recipients)
+        msg['Cc'] = ', '.join(cc_recipients)
+        msg['Subject'] = subject
+        msg.attach(MIMEText(body))
+
+        server = smtplib.SMTP(config['EMAIL']['EMAIL_SERVER'])
+        server.starttls()
+        server.login(login, password)
+        server.sendmail(fromaddr, recipients, msg.as_string())
+        server.quit()
+
