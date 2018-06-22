7048e2f4f6c4c4347b19788d792f2122dfe49ffc
PhiColTan <DamLaR1803@gmail.com>
2018-06-22 11:43:39 -0300

    Add main to main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+# Name: Haar Cascade Creation
+# Desc: Creation of custom Haar Cascades Classifiers
+# Repo: https://github.com/IamPhytan/haar-cascade-creation
+# Author: IamPhytan
+# License: MIT
+# Path: -
+
+import sys
+
+if __name__ == '__main__':
+
+    assert (sys.version_info[0] >= 3), "Python 3 or a more recent version is required."
