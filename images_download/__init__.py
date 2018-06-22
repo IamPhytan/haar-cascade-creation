35bb4bcec0d1129aa38c7c829747e481cc464db7
PhiColTan <DamLaR1803@gmail.com>
2018-06-22 11:49:44 -0300

    Add images_download

---
+++ images_download/__init__.py
@@ -0,0 +1,7 @@
+
+# Name: Haar Cascade Creation
+# Desc: Creation of custom Haar Cascades Classifiers
+# Repo: https://github.com/IamPhytan/haar-cascade-creation
+# Author: IamPhytan
+# License: MIT
+# Path: -/images_download
